ffd7717b7d5aed9018dec098fd3810d83e736ffa
Remedios Ferrandez Simon <fesima@alumni.uv.es>
2026-05-06 17:39:27 +0200

    analisis de arboles filogeneticos

---
+++ results/2026-03-30/guion9.R
@@ -0,0 +1,5 @@
+# Como en la seccion de la practica del aula virtual no está el archivo 
+# del guion de prácticas en fomrato .qmd, he creado un archivo nuevo sobre el 
+# que escribiré los códigos. 
+
+library(phangorn)
